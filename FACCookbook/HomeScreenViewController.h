2386dd62528df095b10219291dec683389c42e49
Aaron Eisses <aaron.eisses@gmail.com>
2015-04-30 22:24:47 -0300

    Began working on the storyboard

---
+++ FACCookbook/HomeScreenViewController.h
@@ -0,0 +1,14 @@
+//
+//  HomeScreenViewController.h
+//  FACCookbook
+//
+//  Created by Aaron Eisses on 2015-04-30.
+//  Copyright (c) 2015 EAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface HomeScreenViewController : UICollectionViewController <UICollectionViewDataSource>
+
+@end
---
+++ FACCookbook/HomeScreenViewController.m
@@ -0,0 +1,49 @@
+//
+//  HomeScreenViewController.m
+//  FACCookbook
+//
+//  Created by Aaron Eisses on 2015-04-30.
+//  Copyright (c) 2015 EAC. All rights reserved.
+//
+
+#import "HomeScreenViewController.h"
+
+@implementation HomeScreenViewController
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma UICollectioView Data Source
+- (NSInteger)collectionView:(UICollectionView *)view numberOfItemsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView: (UICollectionView *)collectionView {
+    return 1;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)cv cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
+#pragma mark - UICollectionViewDelegate
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    // TODO: Deselect item
+}
+@end
+
