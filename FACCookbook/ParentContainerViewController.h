83deb28a9fcae0b7edc033b188fe74315cae3c98
Kirk MacPhee <kmacphee@salesforce.com>
2015-07-22 14:32:37 -0300

    favourite with parent view controller changes

---
+++ FACCookbook/ParentContainerViewController.h
@@ -0,0 +1,18 @@
+//
+//  ParentContainerViewController.h
+//  FACCookbook
+//
+//  Created by Kirk MacPhee on 2015-07-22.
+//  Copyright (c) 2015 EAC. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface ParentContainerViewController : UICollectionViewController <UICollectionViewDataSource, NSFetchedResultsControllerDelegate>
+
+@property (nonatomic,strong) NSManagedObjectContext* managedObjectContext;
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+
+@end
+
---
+++ FACCookbook/ParentContainerViewController.m
@@ -0,0 +1,9 @@
+//
+//  ParentContainerViewController.m
+//  FACCookbook
+//
+//  Created by Kirk MacPhee on 2015-07-22.
+//  Copyright (c) 2015 EAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
