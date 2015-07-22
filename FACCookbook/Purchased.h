9130222f421aa705fe52db8fd984a60103d50cf0
Amisha Goyal <amisha.goyal@amishagoyal-ltm.internal.salesforce.com>
2015-07-22 17:42:03 -0300

    Added Purchased

---
+++ FACCookbook/Purchased.h
@@ -0,0 +1,17 @@
+//
+//  Purchased.h
+//  FACCookbook
+//
+//  Created by Amisha Goyal on 7/22/15.
+//  Copyright (c) 2015 EAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface Purchased : NSManagedObject
+
+@property (nonatomic, retain) NSOrderedSet *recipes;
+@property (nonatomic, retain) NSNumber *purchasedId;
+
+@end
---
+++ FACCookbook/Purchased.m
@@ -0,0 +1,17 @@
+//
+//  Purchased.m
+//  FACCookbook
+//
+//  Created by Amisha Goyal on 7/22/15.
+//  Copyright (c) 2015 EAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Purchased.h"
+
+@implementation Purchased
+
+@dynamic recipes;
+@dynamic purchasedId;
+
+@end
