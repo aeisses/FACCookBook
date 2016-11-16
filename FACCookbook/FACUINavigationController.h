1436eeec8333f97c7ecb8e7d63659660d03f9c0c
Aaron Eisses <aaron.eisses@gmail.com>
2016-11-16 19:47:47 -0400

    Fixed the landscape issues

---
+++ FACCookbook/FACUINavigationController.h
@@ -0,0 +1,14 @@
+//
+//  FACUINavigationController.h
+//  FACCookbook
+//
+//  Created by Aaron Eisses on 2016-11-16.
+//  Copyright © 2016 EAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FACUINavigationController : UINavigationController
+
+@end
